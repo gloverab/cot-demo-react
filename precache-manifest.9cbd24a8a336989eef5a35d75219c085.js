2998d22fc01eee45dca0ff3f0405c4d349f9a2a0
Alex Glover <alex@circulation.com>
2020-10-14 23:05:06 -0400

    Updates

---
+++ precache-manifest.9cbd24a8a336989eef5a35d75219c085.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd73660b484d1c18cf7559f08f54ba9",
+    "url": "/cot-demo-react/index.html"
+  },
+  {
+    "revision": "b5b9e2f73711cafaf38e",
+    "url": "/cot-demo-react/static/css/main.6ae5151e.chunk.css"
+  },
+  {
+    "revision": "021a7ffd9c0bef5e1935",
+    "url": "/cot-demo-react/static/js/2.024597b7.chunk.js"
+  },
+  {
+    "revision": "8c148efb6436185179e1b7ec9806bfdd",
+    "url": "/cot-demo-react/static/js/2.024597b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5b9e2f73711cafaf38e",
+    "url": "/cot-demo-react/static/js/main.94957421.chunk.js"
+  },
+  {
+    "revision": "8a7856894874e8cb8194",
+    "url": "/cot-demo-react/static/js/runtime-main.6073c798.js"
+  }
+]);
